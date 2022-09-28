789f64813523576598128aed13c82a81fa5b5f4e
jiruneko <oohikaru310@gmail.com>
2022-09-29 05:48:39 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+$(function(){
+    $('input#search').quicksearch('table tbody tr');
+  });
+
+$(function(){
+    $('.js-modal-open').each(function(){
+        $(this).on('click',function(){
+            var target = $(this).data('target');
+            var modal = document.getElementById(target);
+            $(modal).fadeIn();
+            return false;
+        });
+    });
+    $('.js-modal-close').on('click',function(){
+        $('.js-modal').fadeOut();
+        return false;
+    }); 
+});
